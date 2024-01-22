d5feed9421576313af1ce412584b08df950561e6
Kelechi <laviedegeorge1292@gmail.com>
2024-01-22 16:05:16 +0100

    create endpoints in ninjas contoller and add dto

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -9,6 +20,31 @@ export class NinjasController {
 
   @Get(':id')
   getNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  @Put(':id')
+  updateNinja(
+    @Param(':id') id: string,
+    @Body() updateNinjaDto: UpdateNinjaDto,
+  ) {
+    return {
+      id,
+      name: updateNinjaDto,
+    };
+  }
+
+  @Delete(':id')
+  deleteNinja(@Param(':id') id: string) {
     return {
       id,
     };
