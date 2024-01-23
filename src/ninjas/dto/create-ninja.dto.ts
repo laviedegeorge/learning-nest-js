4cf175cb9f3ee5bb7e135a9be4183fb9b59de31b
Kelechi <laviedegeorge1292@gmail.com>
2024-01-23 12:08:29 +0100

    add pipes for validation

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,5 +1,9 @@
+import { IsEnum, MinLength } from 'class-validator';
+
 export class CreateNinjaDto {
   id: number;
+  @MinLength(3)
   name: string;
+  @IsEnum(['stars', 'nunchucks'], { message: 'Enter a correct weapon' })
   weapon: 'stars' | 'nunchucks';
 }
--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -5,9 +5,11 @@ import {
   Get,
   NotFoundException,
   Param,
+  ParseIntPipe,
   Post,
   Put,
   Query,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
@@ -23,27 +25,30 @@ export class NinjasController {
   }
 
   @Get(':id')
-  getNinja(@Param('id') id: string) {
+  getNinja(@Param('id', ParseIntPipe) id: number) {
     try {
-      return this.ninjaService.getNinja(+id);
+      return this.ninjaService.getNinja(id);
     } catch (error) {
       throw new NotFoundException();
     }
   }
 
   @Post()
-  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+  createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto) {
     return this.ninjaService.createNinja(createNinjaDto);
   }
 
   @Put(':id')
-  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+  updateNinja(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(new ValidationPipe()) updateNinjaDto: UpdateNinjaDto,
+  ) {
     console.log(id, +id);
-    return this.ninjaService.updateNinja(+id, updateNinjaDto);
+    return this.ninjaService.updateNinja(id, updateNinjaDto);
   }
 
   @Delete(':id')
-  deleteNinja(@Param('id') id: string) {
-    return this.ninjaService.removeNinja(+id);
+  deleteNinja(@Param('id', ParseIntPipe) id: number) {
+    return this.ninjaService.removeNinja(id);
   }
 }
